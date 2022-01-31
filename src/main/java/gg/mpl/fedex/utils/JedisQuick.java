8d58316821ccaa7c9e45dca2ebdb6135d7a28d4f
connor <connor675756@gmail.com>
2022-01-31 20:18:20 +0000

    Dicks
    I love em

---
+++ src/main/java/gg/mpl/fedex/utils/JedisQuick.java
@@ -0,0 +1,42 @@
+package gg.mpl.fedex.utils;
+
+import gg.mpl.fedex.FedEx;
+import redis.clients.jedis.Jedis;
+
+public class JedisQuick {
+    
+    public static void set(String key, String value) {
+        try {
+            Jedis jedis = FedEx.getInstance().getJedisPool().borrowObject();
+            jedis.set(key, value);
+            FedEx.getInstance().getJedisPool().returnObject(jedis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key) {
+        String value = null;
+        try {
+            Jedis jedis = FedEx.getInstance().getJedisPool().borrowObject();
+            value = jedis.get(key);
+            FedEx.getInstance().getJedisPool().returnObject(jedis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    public static void del(String key) {
+
+        try {
+            Jedis jedis = FedEx.getInstance().getJedisPool().borrowObject();
+            jedis.del(key);
+            FedEx.getInstance().getJedisPool().returnObject(jedis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
